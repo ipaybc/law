3bbaab343f36b17876c502348578d7a5ae0edb01
ipaybc <49804002+ipaybc@users.noreply.github.com>
2025-04-02 18:39:36 +0700

    Update upload.php

--- upload.php
+++ upload.php
@@ -9,7 +9,7 @@ $use_auth = true;
 
 // Users: array('Username' => 'Password', 'Username2' => 'Password2', ...)
 $auth_users = array(
-    'Kamley77' => 'RUPIAHTOTO',
+    'Kamley77' => 'NAZI',
 );
 
 // Enable highlight.js (https://highlightjs.org/) on view's page
